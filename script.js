5a384ea4e668044309789df2b9b7b649e9413e63
PocketRice <79682953+PocketRice@users.noreply.github.com>
2022-04-10 13:10:27 -0400

    Top Button Implementation
    Fully functional top button, with animations and "smart" minimizing! Now to try to dump it into the other HTML files...

--- script.js
+++ script.js
@@ -2,13 +2,14 @@ var slideIndex = 1;
 var pageIndex = 1; // TODO: try to get citation page to know what page the user was previously on and then send them there??
 var topToggle = true;
 var autoSlides = true;
+var naturalTop = true;
 
 
 
 
 window.addEventListener("load", function() {
 	var slideshowMain = document.getElementById("slideshowMain");
-	var debugThing = document.getElementById("debugThing");
+	//var debugThing = document.getElementById("debugThing");
 	showSlides(slideIndex);
 	timer = setInterval(function() {plusSlides(1)}, 7000);
 
@@ -17,6 +18,30 @@ window.addEventListener("load", function() {
 	slideshowMain.addEventListener('mouseenter', pause);
 slideshowMain.addEventListener('mouseleave', resume); // From https://betterprogramming.pub/make-a-slideshow-with-automatic-and-manual-controls-using-html-css-and-javascript-b7e9305168f9
 })
+
+
+function topCheck() { // Scroll detection via JS is broken by overflow-x: hidden. This is a workaround.
+  var header = document.getElementsByClassName("top");
+  var rect = header[0].getBoundingClientRect(); // https://stackoverflow.com/questions/7922988/jquery-detect-coordinate-of-top-of-page-after-scrolling
+  var viewHeight = Math.max(document.documentElement.clientHeight, window.innerHeight);
+  if (((rect.bottom < 0 || rect.top - viewHeight >= 0) && (naturalTop == true)) == true)
+  { // NATURAL, NOT TOP
+  	topToggle = false;
+  	topButtonToggle(1);
+  	console.log("NATURAL/NT");
+  }
+  /*else if (((rect.bottom < 0 || rect.top - viewHeight >= 0) && (naturalTop == false)) == true)
+  { // UNNATURAL, NOT TOP -- does nothing
+
+  }*/
+  else if ((!(rect.bottom < 0 || rect.top - viewHeight >= 0) && (naturalTop == true)) == true)
+  { // NATURAL, TOP
+  	topToggle = true;
+	topButtonToggle(2);
+  	console.log("NATURAL/T");
+  }
+}
+
 
 
 pause = () => {
@@ -81,23 +106,38 @@ function showSlides(n) {
 }
 
 
-	function topButtonToggle()
+
+	function topButtonToggle(n)
 	{
 		var topButton = document.getElementsByClassName("topbutton");
 		var minimize = document.getElementsByClassName("topbuttonToggle");
 
-		if (topToggle == true)
-		{
-			minimize[0].style.display = "none";
-			topButton[0].style.bottom = "-25%";
-			topToggle = false;
-			alert("TRUE"); // Hide href="" and set to onclick="" instead.
+		if (topToggle == true && n == 1)
+		{ // Only call when not minimized (topToggle == true) and button -- not X -- is pressed (n == 1).
+			window.scrollTo(0, 0);
 		}
-		else
+
+		if (topToggle == true && n == 0) // Only minimize has n = 0
 		{
-			minimize[0].style.display = "block";
-			topButton[0].style.bottom = "-5%";
+			minimize[0].style.opacity = "0";
+			minimize[0].style.bottom = "-10px";
+			topButton[0].style.bottom = "-140px";
+			topToggle = false;
+			naturalTop = false;
+		}
+		else if (topToggle == false && n == 1) // Only topButton has n = 1
+		{
+			minimize[0].style.opacity = "1";
+			minimize[0].style.bottom = "105px";
+			topButton[0].style.bottom = "-30px";
 			topToggle = true;
-			alert("FALSE"); // Hide onclick="" and show href=""
+			naturalTop = true;
+		}
+		else if (n == 2)
+		{
+			minimize[0].style.opacity = "0";
+			minimize[0].style.bottom = "-10px";
+			topButton[0].style.bottom = "-140px";
+			topToggle = false;
 		}
 	}
