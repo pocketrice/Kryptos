9780de73e487134754fe6c07889b89015481b1f2
PocketRice <79682953+PocketRice@users.noreply.github.com>
2022-04-10 23:46:23 -0400

    v1.0.1 Release
    What a day! Cipher page cleaned up, so much polishing, better JS, nice "to top" button, and so much more. Good work -- up next, resources.html and some tiny polishing and you're all done!!

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
+// PROJECT 3: CAREER PAGE | Lucas Xie | P4A Web Programming
+// Description: The Javascript for this website. It is used for the interactive slideshow, "to top" button, and other small 'interactive' aspects.
+// An abbreviated version of this is used on all the pages except for homepage, as those don't need any of the slideshow code.
+
 var slideIndex = 1;
-var pageIndex = 1; // TODO: try to get citation page to know what page the user was previously on and then send them there??
+var pageIndex = 1; // Scrapped idea; supposed to take you to the previous page you were on from citations.html
 var topToggle = true;
 var autoSlides = true;
 var naturalTop = true;
@@ -21,7 +25,7 @@ slideshowMain.addEventListener('mouseleave', resume); // From https://betterprog
 
 
 function topCheck() { // Scroll detection via JS is broken by overflow-x: hidden. This is a workaround.
-  var header = document.getElementsByClassName("top");
+	var header = document.getElementsByClassName("top");
   var rect = header[0].getBoundingClientRect(); // https://stackoverflow.com/questions/7922988/jquery-detect-coordinate-of-top-of-page-after-scrolling
   var viewHeight = Math.max(document.documentElement.clientHeight, window.innerHeight);
   if (((rect.bottom < 0 || rect.top - viewHeight >= 0) && (naturalTop == true)) == true)
@@ -30,16 +34,15 @@ function topCheck() { // Scroll detection via JS is broken by overflow-x: hidden
   	topButtonToggle(1);
   	console.log("NATURAL/NT");
   }
-  /*else if (((rect.bottom < 0 || rect.top - viewHeight >= 0) && (naturalTop == false)) == true)
-  { // UNNATURAL, NOT TOP -- does nothing
 
-  }*/
   else if ((!(rect.bottom < 0 || rect.top - viewHeight >= 0) && (naturalTop == true)) == true)
   { // NATURAL, TOP
   	topToggle = true;
-	topButtonToggle(2);
+  	topButtonToggle(2);
   	console.log("NATURAL/T");
   }
+
+  // UNNATURAL/TOP and UNNATURAL/NOT TOP don't do anything.
 }
 
 
@@ -74,13 +77,13 @@ function showSlides(n) {
 
 	if (n > slides.length) {slideIndex = 1} // When n exceeds length, wrap back around
 		if (n < 1) {slideIndex = slides.length}
-	for (i = 0; i < slides.length; i++) // For loop
-	{
-		slides[i].style.opacity = "0";
-		slidesCaption[i].style.opacity = "0";
-	}
+			for (i = 0; i < slides.length; i++)
+			{
+				slides[i].style.opacity = "0";
+				slidesCaption[i].style.opacity = "0";
+			}
 
-/*
+/* // Preload the previous image so when the gallery transitions the background isn't showing through 
 	slideIndexPreload = n-2;
 
 	if (slideIndexPreload <= 0)
@@ -98,7 +101,7 @@ function showSlides(n) {
 
 
 	slides[slideIndexPreload].style.opacity = "1";
-	slides[slideIndexPreload].style.zIndex = "-10"; FIX */
+	slides[slideIndexPreload].style.zIndex = "-10"; */
 
 
 	slides[slideIndex-1].style.opacity = "1";
@@ -107,17 +110,17 @@ function showSlides(n) {
 
 
 
-	function topButtonToggle(n)
-	{
-		var topButton = document.getElementsByClassName("topbutton");
-		var minimize = document.getElementsByClassName("topbuttonToggle");
+function topButtonToggle(n)
+{
+	var topButton = document.getElementsByClassName("topbutton");
+	var minimize = document.getElementsByClassName("topbuttonToggle");
 
-		if (topToggle == true && n == 1)
+	if (topToggle == true && n == 1)
 		{ // Only call when not minimized (topToggle == true) and button -- not X -- is pressed (n == 1).
 			window.scrollTo(0, 0);
 		}
 
-		if (topToggle == true && n == 0) // Only minimize has n = 0
+		if (topToggle == true && n == 0) // Only minimize has n = 0; minimize the button
 		{
 			minimize[0].style.opacity = "0";
 			minimize[0].style.bottom = "-10px";
@@ -125,7 +128,7 @@ function showSlides(n) {
 			topToggle = false;
 			naturalTop = false;
 		}
-		else if (topToggle == false && n == 1) // Only topButton has n = 1
+		else if (topToggle == false && n == 1) // Only topButton has n = 1; maximize the button
 		{
 			minimize[0].style.opacity = "1";
 			minimize[0].style.bottom = "105px";
@@ -133,7 +136,7 @@ function showSlides(n) {
 			topToggle = true;
 			naturalTop = true;
 		}
-		else if (n == 2)
+		else if (n == 2) // Special case where button is minimized but still natural (was not manually minimized)
 		{
 			minimize[0].style.opacity = "0";
 			minimize[0].style.bottom = "-10px";
